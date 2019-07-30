04ec86579d72a84215e00c5fd9fcc5e73524139a
Maribel Fonseca <maribel@guegue.net>
2019-07-30 10:15:29 +0000

    fix country name lookup

--- models/Organization.php
+++ models/Organization.php
@@ -27,16 +27,19 @@ class Organization extends base\Organization
 
     public function getCountryNameText()
     {
-        if ($this->country0)
-            return $this->country0->name;
+        if ($this->country_id)
+            return $this->country_id;
+        # we no longer use data_list
+        #if ($this->country0)
+        #    return $this->country0->name;
         return "";
     }
 
     public function getCountryName()
     {
         $countries = UCatalogo::listCountries();
-        if ($this->country && isset($countries[$this->country]))
-            return $countries[$this->country];
+        if ($this->country_id && isset($countries[$this->country_id]))
+            return $countries[$this->country_id];
         return "";
     }
 
