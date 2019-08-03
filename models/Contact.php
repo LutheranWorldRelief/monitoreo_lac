f7e722def2e8246091ff8e17ce14614e24fb9cbb
fernandojose95 <josefernandohernandezpaz@gmail.com>
2019-08-03 12:01:08 -0600

    eliminando lineas comentadas y try catch en Contact

--- models/Contact.php
+++ models/Contact.php
@@ -59,43 +59,32 @@ class Contact extends base\Contact
             }
             $contact->organization_id = $org->id;
         }
+
         $contact->save();
-//        var_dump();
-//        if ($contact->save()) {
-        try {
 
+        $idContact = $contact->id;
+        $projectContact = ProjectContact::find()->andFilterWhere(['project_id' => $project_id, 'contact_id' => $idContact])->one();
 
-            $idContact = $contact->id;
-            $projectContact = ProjectContact::find()->andFilterWhere(['project_id' => $project_id, 'contact_id' => $idContact])->one();
-
-            if (is_null($projectContact)) {
-                $projectContact = new ProjectContact();
-            }
-
-            $product = MonitoringProduct::getSpecificProduct($data['product']);
-            $product_id = (int)$product->id;
-
-
-
-            $projectContact->project_id = $project_id;
-            $projectContact->contact_id = $idContact;
-            $projectContact->product_id = $product_id;
-            $projectContact->area = (int)$data['area'];
-            $projectContact->development_area = (int)$data['development_area'];
-            $projectContact->productive_area = (int)$data['productive_area'];
-            $projectContact->age_development_plantation = (int)$data['age_development_plantation'];
-            $projectContact->age_productive_plantation = (int)$data['age_productive_plantation'];
-            $projectContact->date_entry_project = $data['date_entry_project'];
-            $projectContact->yield = (int)$data['yield'];
-            $projectContact->date_end_project = null;
-            $projectContact->save();
-
-        } catch (\Error $error) {
-            var_dump($error);
+        if (is_null($projectContact)) {
+            $projectContact = new ProjectContact();
         }
-//            if ($proyecto->save())
-//                return $idContact;
-//        }
+
+        $product = MonitoringProduct::getSpecificProduct($data['product']);
+        $product_id = (int)$product->id;
+
+
+        $projectContact->project_id = $project_id;
+        $projectContact->contact_id = $idContact;
+        $projectContact->product_id = $product_id;
+        $projectContact->area = (int)$data['area'];
+        $projectContact->development_area = (int)$data['development_area'];
+        $projectContact->productive_area = (int)$data['productive_area'];
+        $projectContact->age_development_plantation = (int)$data['age_development_plantation'];
+        $projectContact->age_productive_plantation = (int)$data['age_productive_plantation'];
+        $projectContact->date_entry_project = $data['date_entry_project'];
+        $projectContact->yield = (int)$data['yield'];
+        $projectContact->date_end_project = null;
+        $projectContact->save();
 
         return $idContact;
     }
