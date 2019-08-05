d874e8527f1769001adc450279789ae80f59ab7a
roberto <robertohernandez9@gmail.com>
2019-08-05 17:40:36 -0400

    moficacion de regla en modelo attendance

--- models/Attendance.php
+++ models/Attendance.php
@@ -75,12 +75,16 @@ class Attendance extends base\Attendance
                 }
             }
 
-            $return &= parent::save();
-
-            if (!$return) {
-                throw new Exception("No se logró guardar el registro de participante");
+            if(!parent::save())
+            {
+                print_r(parent::getAttributes());
+                print_r(parent::getErrors());
+                exit();
             }
 
+            /*  if (!$return) {
+                  throw new Exception("No se logró guardar el registro de participante");
+              }*/
             $transaction->commit();
         } catch (Exception $e) {
             $transaction->rollBack();
@@ -103,7 +107,7 @@ class Attendance extends base\Attendance
                 [['community'], 'string', 'max' => 255],
             ],
             [
-                [['fullname'], 'required'],
+                /*  [['fullname'], 'required'],*/
                 [['contact_id', 'organization_id', 'org_name', 'document', 'sex', 'country_id', 'community', 'phone_personal', 'event_id', 'type_id'], 'safe'],
             ]
         );
