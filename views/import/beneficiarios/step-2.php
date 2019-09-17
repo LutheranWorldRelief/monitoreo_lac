c52172451713dbf6059abd9ee6e57e94dec2b72c
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2019-09-17 15:20:05 -0600

    agregando stings para que sean traducidas

--- views/import/beneficiarios/step-2.php
+++ views/import/beneficiarios/step-2.php
@@ -17,20 +17,20 @@ $form = ActiveForm::begin(['options' => ['enctype' => "multipart/form-data",]]);
 <?= Html::hiddenInput("guardar", true); ?>
 <div class="row">
     <div class="col-lg-4 col-lg-offset-1">
-    <h3 class="text-info pull-left"><?= \Yii::t('app', 'Paso 2: Ajustar Datos')?></h3>
+        <h3 class="text-info pull-left"><?= \Yii::t('app', 'Paso 2: Ajustar Datos') ?></h3>
     </div>
     <div class="col-lg-7">
         <ul class="list-inline pull-right">
             <li>
                 <a class="btn btn-danger pull-left" href="<?= Url::to(['import/beneficiarios-paso1']) ?>">
-		<i class="fa fa-backward"></i><?= \Yii::t('app', 'Cancelar')?>
+                    <i class="fa fa-backward"></i><?= \Yii::t('app', 'Cancelar') ?>
                 </a>
             </li>
             <?php if (count($data['Correcto']) > 0): ?>
                 <li>
                     <button type="submit" class="btn btn-success btn-block pull-right"><i
                                 class="fa fa-upload fa-w-16"></i>
-		<?= \Yii::t('app', 'Importar y continuar')?>
+                        <?= \Yii::t('app', 'Importar y continuar') ?>
                     </button>
                 </li>
             <?php endif; ?>
@@ -53,7 +53,7 @@ if (count($data['Guardar']) < 1):
     ?>
     <div class="row">
         <div class="col-lg-10 col-lg-offset-1 alert alert-danger">
-	<?= \Yii::t('app', 'No se importará ningún registro')?>
+            <?= \Yii::t('app', 'No se importará ningún registro') ?>
         </div>
     </div>
 <?php endif; ?>
@@ -61,7 +61,7 @@ if (count($data['Guardar']) < 1):
 
 <div class="row">
     <div class="col-lg-4 col-lg-offset-1">
-	<?= \Yii::t('app', 'País de la importación')?>
+        <?= \Yii::t('app', 'País de la importación') ?>
         <?=
         Select2::widget([
             'name' => "pais",
@@ -80,19 +80,20 @@ if (count($data['Guardar']) < 1):
     <div class="col-lg-8 col-lg-offset-3">
         <ul class="nav nav-pills" id="myTabs" role="tablist">
             <li role="presentation" class="active">
-	    <a href="#crear" id="home-tab" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true"><?= \Yii::t('app', 'Registros a Crear')?></a>
+                <a href="#crear" id="home-tab" role="tab" data-toggle="tab" aria-controls="home"
+                   aria-expanded="true"><?= \Yii::t('app', 'Registros a Crear') ?></a>
             </li>
             <?php if (count($data['Guardar']) > 0): ?>
                 <li role="presentation" class="">
                     <a href="#correcto" role="tab" id="profile-tab" data-toggle="tab" aria-controls="profile"
-		    aria-expanded="false"><?= \Yii::t('app', 'Datos a Importar')?></a>
+                       aria-expanded="false"><?= \Yii::t('app', 'Datos a Importar') ?></a>
                 </li>
             <?php endif; ?>
             <?php if (count($data['Incorrecto']) > 0): ?>
                 <li role="presentation" class="">
                     <div class="alert alert-danger">
                         <a href="#errores" role="tab" id="profile-tab" data-toggle="tab" aria-controls="profile"
-			aria-expanded="false"><?= \Yii::t('app', 'Datos que no se importarán')?></a>
+                           aria-expanded="false"><?= \Yii::t('app', 'Datos que no se importarán') ?></a>
                     </div>
                 </li>
             <?php endif; ?>
@@ -101,17 +102,17 @@ if (count($data['Guardar']) < 1):
 </div>
 <div class="tab-content" id="myTabContent">
     <div class="tab-pane fade active in" role="tabpanel" id="crear" aria-labelledby="home-tab">
-    <h4 class="text-info" style="padding: 20px"><?= \Yii::t('app', 'Registros a Crear')?></h4>
+        <h4 class="text-info" style="padding: 20px"><?= \Yii::t('app', 'Registros a Crear') ?></h4>
         <div class="row" style="padding: 20px">
             <div class="col-lg-12">
                 <div class="row">
                     <div class="col-lg-6 table-responsive">
-		    <h3><?= \Yii::t('app', 'Proyectos')?></h3>
+                        <h3><?= \Yii::t('app', 'Proyectos') ?></h3>
                         <table class="table table-bordered">
                             <thead>
                             <tr>
-			    <th><?= \Yii::t('app', 'Código')?></th>
-			    <th><?= \Yii::t('app', 'Nombre')?></th>
+                                <th><?= \Yii::t('app', 'Código') ?></th>
+                                <th><?= \Yii::t('app', 'Nombre') ?></th>
                             </tr>
                             </thead>
                             <tbody>
@@ -125,12 +126,12 @@ if (count($data['Guardar']) < 1):
                         </table>
                     </div>
                     <div class="col-lg-6 table-responsive">
-		    <h3><?= \Yii::t('app', 'Organizaciones')?></h3>
+                        <h3><?= \Yii::t('app', 'Organizaciones') ?></h3>
                         <table class="table table-bordered">
                             <thead>
                             <tr>
-			    <th><?= \Yii::t('app', 'Organización')?></th>
-			    <th><?= \Yii::t('app', 'Vincular con')?></th>
+                                <th><?= \Yii::t('app', 'Organización') ?></th>
+                                <th><?= \Yii::t('app', 'Vincular con') ?></th>
                             </tr>
                             </thead>
                             <tbody>
@@ -161,7 +162,7 @@ if (count($data['Guardar']) < 1):
                 </div>
                 <div class="row">
                     <div class="col-lg-6 table-responsive">
-                        <h3>Paises</h3>
+                        <h3><?= \Yii::t('app', 'Paises') ?></h3>
                         <table class="table table-bordered">
                             <?php foreach ($paisesRegistrar as $d): ?>
                                 <tr>
@@ -172,7 +173,7 @@ if (count($data['Guardar']) < 1):
 
                     </div>
                     <div class="col-lg-6 table-responsive">
-                        <h3>Educación</h3>
+                        <h3><?= \Yii::t('app', 'Educación') ?></h3>
                         <table class="table table-bordered">
                             <?php $key = 0;
                             foreach ($educacionRegistrar as $d): ?>
@@ -207,7 +208,7 @@ if (count($data['Guardar']) < 1):
             <div class="col-lg-11 col-lg-offset-0">
                 <div class="row">
                     <div class="col-lg-12 table-responsive">
-                        <h3>Datos a importar</h3>
+                        <h3><?= \Yii::t('app', 'Datos a importar') ?></h3>
                         <?php if (count($data['Guardar']) > 0) echo UString::array2Table($data['Guardar']); ?>
                     </div>
                 </div>
@@ -221,8 +222,8 @@ if (count($data['Guardar']) < 1):
                 <?php if (count($data['Incorrecto']) > 0): ?>
                     <div class="row">
                         <div class="col-lg-12 table-responsive">
-                            <h3>Datos Incorrectos</h3>
-			    <div class="alert alert-error"><?= \Yii::t('app', 'Verifique datos de proyecto, organización implementadora y beneficiario para:')?>
+                            <h3><?= \Yii::t('app', 'Datos Incorrectos') ?></h3>
+                            <div class="alert alert-error"><?= \Yii::t('app', 'Verifique datos de proyecto, organización implementadora y beneficiario para:') ?>
                             </div>
                             <table class="table table-bordered">
                                 <?php foreach ($data['Incorrecto'] as $d): ?>
@@ -242,13 +243,13 @@ if (count($data['Guardar']) < 1):
 <ul class="list-inline pull-right">
     <li>
         <a class="btn btn-danger pull-left" href="<?= Url::to(['import/beneficiarios-paso1']) ?>">
-	<i class="fa fa-backward"></i><?= \Yii::t('app', 'Cancelar')?>
+            <i class="fa fa-backward"></i><?= \Yii::t('app', 'Cancelar') ?>
         </a>
     </li>
     <?php if (count($data['Guardar']) > 0): ?>
         <li>
             <button type="submit" class="btn btn-success btn-block pull-right"><i class="fa fa-upload fa-w-16"></i>
-	<?= \Yii::t('app', 'Importar y continuar')?>
+                <?= \Yii::t('app', 'Importar y continuar') ?>
             </button>
         </li>
     <?php endif; ?>
